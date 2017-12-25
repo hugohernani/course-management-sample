75431640bf159a7ae9f3fc504c5c1e543b32ee68
Hugo Hernani <hugo@quezmedia.com>
2017-12-25 12:16:34 -0300

    Improve jsonapi spec matchers to handle collection responses

--- spec/support/matchers/jsonapi_match.rb
+++ spec/support/matchers/jsonapi_match.rb
@@ -1,3 +1,23 @@
+RSpec::Matchers.define :be_collection_jsonapi_match do |model|
+  match do |actual|
+    parsed_actual = JSON.parse(actual, symbolize_names: true) rescue actual
+    actual_data = parsed_actual.dig(:data).try(:first)
+
+    actual_data.dig(:type) == model && actual_data.dig(:attributes).is_a?(Hash) &&
+      parsed_actual.dig(:links).is_a?(Hash)
+  end
+end
+
+RSpec::Matchers.define :be_collection_assoc_jsonapi_match do |model|
+  match do |actual|
+    parsed_actual = JSON.parse(actual, symbolize_names: true) rescue actual
+    actual_data = parsed_actual.dig[:data].try(:first)
+
+    actual_data.dig(:type) == model && actual_data.dig(:attributes).is_a?(Hash) &&
+      actual_data.dig(:relationships).is_a?(Hash) && parsed_actual.dig(:links).is_a?(Hash)
+  end
+end
+
 RSpec::Matchers.define :be_jsonapi_match do |model|
   match do |actual|
     parsed_actual = JSON.parse(actual, symbolize_names: true) rescue actual
