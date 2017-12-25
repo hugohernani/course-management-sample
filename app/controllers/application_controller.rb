32bc616acd30482158809110005c0676f423d2f8
Hugo Hernani <hugo@quezmedia.com>
2017-12-25 01:45:37 -0300

    Add default error json api spec response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def respond_with_errors(object)
+    render json: {errors: ErrorSerializer.serialize(object)}, status: :unprocessable_entity
+  end
 end
