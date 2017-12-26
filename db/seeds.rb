875873d42c30fcdd96c5cc3e4e12a3c7b23b1723
Hugo Hernani <hugo@quezmedia.com>
2017-12-26 16:10:52 -0300

    Add some data to be seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+teacher1 = Teacher.create(name: 'Jack')
+teacher2 = Teacher.create(name: 'Louis')
+student1 = Student.create(name: 'Peter', email: 'peter@fractal-technology.com')
+student2 = Student.create(name: 'John', email: 'john@fractal-technology.com')
+
+rails_course = Course.create(title: 'Rails API',
+  description: 'Rails API development')
+
+rails_course.teachers << [teacher1, teacher2]
+rails_course.students << [student1, student2]
+
+5.times do |i|
+  FactoryBot.create(:course, :with_teachers, :with_students)
+end
